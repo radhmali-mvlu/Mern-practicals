56ae19e141d6e773815ad684f3e2f8acb38433db
radhika mali <radhmalimvlu@gmail.com>
2026-08-12 19:39:39 +0530

    Update Profile.jsx

--- practical5A, B &C/src/Profile.jsx
+++ practical5A, B &C/src/Profile.jsx
@@ -3,7 +3,7 @@ function Profile() {
         <div>
             <h1>Student Profile</h1>
             <hr/>
-            <p>Name: Asmitha</p>
+            <p>Name: Radhika Mali</p>
             <p>Course: B.Sc. IT</p>
             <p>CGPA:9.55 </p>
             <p>Skills: JavaScript, Python, .NET, REACT, Node.js</p>
